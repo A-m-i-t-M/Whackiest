8a3ef62afb73ec33369d177a3ec379d9f7fd2f6e
A-m-i-t-M <amitnoreply63@gmail.com>
2024-12-19 20:20:31 +0530

    Rectified my mistake

---
+++ Admin/ActualFrontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Home from './pages/Home'
+import Details from './pages/Details'
+
+export default function App() {
+  return <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/review' element={<Details/>}/>
+        <Route path='/home' element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
+  </>
+}
