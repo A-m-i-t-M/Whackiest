c18343373abff1c62a059604bb58f85ea8cbd5fa
A-m-i-t-M <amitnoreply63@gmail.com>
2024-12-20 14:03:55 +0530

    home page of client is done basic wala

---
+++ Testing Folder/ClientHome.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function ClientHome() {
+  return <>
+    <div>
+      <h1>Welcome to Pooja Pehal</h1>
+      <div>
+        <div>Div 1</div>
+        <div>Div 2</div>
+        <div>Div 3</div>
+      </div>
+    </div>
+  </>
+}
