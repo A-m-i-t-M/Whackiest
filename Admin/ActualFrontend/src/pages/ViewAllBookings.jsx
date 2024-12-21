0de8349caa96d97955ac5a5c1b0401917f5ac4db
A-m-i-t-M <amitnoreply63@gmail.com>
2024-12-21 15:08:46 +0530

    able to delete the services now from the view bookings for the Client side

--- Admin/ActualFrontend/src/pages/ViewAllBookings.jsx
+++ Admin/ActualFrontend/src/pages/ViewAllBookings.jsx
@@ -5,25 +5,40 @@ import { useNavigate } from "react-router-dom";
 
 
 export default function ViewAllBookings() {
-  const [bookings, setBookings] = useState([]);
+  const [darshanbookings, setDarshanBookings] = useState([]);
+  const [servicebookings, setServiceBookings] = useState([]);
   const [error, setError] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
-    const fetchBookings = async () => {
+    const fetchDarshanBookings = async () => {
       try {
         const response = await fetch("backend/bhakt/darshan/");
         if (!response.ok) {
           throw new Error("Failed to fetch bookings");
         }
         const data = await response.json();
-        setBookings(data.darshans);
+        setDarshanBookings(data.darshans);
       } catch (err) {
         setError(err.message);
       }
     };
 
-    fetchBookings();
+    const fetchServiceBookings = async()=>{
+      try{
+        const response = await fetch("backend/bhakt/booking/");
+        if (!response.ok) {
+          throw new Error("Failed to fetch bookings");
+        }
+        const data = await response.json();
+        setServiceBookings(data.bookings);
+      } catch (err) {
+        setError(err.message);
+      }
+    }
+
+    fetchDarshanBookings();
+    fetchServiceBookings();
     // handleDeleteDarshan();
   }, []);
 
@@ -54,8 +69,31 @@ export default function ViewAllBookings() {
         throw new Error("Delete nahi kar paya bro");
       }
 
-      setBookings((prevBookings)=>
+      setDarshanBookings((prevBookings)=>
         prevBookings.filter((booking)=>booking._id !== darshanId)
+      )
+
+    }catch(error){
+      setError(error.message);
+    }
+  }
+
+  const handleDeleteService = async(bookingId)=>{
+    try{
+      const res = await fetch("/backend/bhakt/booking/delete", {
+        method: "POST",
+        headers: {
+          "Content-Type" : "application/json",
+        },
+        body: JSON.stringify({bookingId})
+      });
+
+      if(!res.ok){
+        throw new Error("Delete nahi kar paya bro");
+      }
+
+      setServiceBookings((prevBookings)=>
+        prevBookings.filter((booking)=>booking._id !== bookingId)
       )
 
     }catch(error){
@@ -86,8 +124,9 @@ export default function ViewAllBookings() {
             <p>Error: {error}</p>
           </div>
         )}
+        <h1 className=" my-10 font-bold text-2xl">Darshans:</h1>
         <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 mt-10">
-          {bookings.map((booking, index) => (
+          {darshanbookings.map((booking, index) => (
             <div
               key={index}
               className={`relative shadow-lg rounded-lg p-6 transition duration-300 border-l-4 ${getCardStyles(
@@ -110,6 +149,40 @@ export default function ViewAllBookings() {
               <button
                 className="relative z-10 mt-4 bg-red-500 text-white text-sm font-medium py-2 px-4 rounded hover:bg-red-600 transition"
                 onClick={()=>handleDeleteDarshan(booking._id)}
+              >
+                Delete
+              </button>
+            </div>
+          ))}
+        </div>
+        <h1 className=" my-10 font-bold text-2xl">Services:</h1>
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 mt-10">
+          {servicebookings.map((booking, index) => (
+            <div
+              key={index}
+              className={`relative shadow-lg rounded-lg p-6 transition duration-300 border-l-4 ${getCardStyles(
+                booking.mandirName
+              )} group`}
+            >
+              <div
+                className="absolute inset-0 bg-cover bg-center opacity-0 group-hover:opacity-40 rounded-lg"
+                style={{ backgroundImage: `url(${images})` }}
+              ></div>
+              <h2 className="text-xl font-semibold text-gray-800 mb-2 relative z-10">
+                Mandir Name: {booking.mandirName}
+              </h2>
+              <p className="text-gray-800 relative z-10">
+                <span className="font-medium">Pandit:</span> {booking.pandit}
+              </p>
+              <p className="text-gray-800 relative z-10">
+                <span className="font-medium">Service:</span> {booking.service}
+              </p>
+              <p className="text-gray-800 relative z-10">
+                <span className="font-medium">Date:</span> {booking.date}
+              </p>
+              <button
+                className="relative z-10 mt-4 bg-red-500 text-white text-sm font-medium py-2 px-4 rounded hover:bg-red-600 transition"
+                onClick={()=>handleDeleteService(booking._id)}
               >
                 Delete
               </button>
