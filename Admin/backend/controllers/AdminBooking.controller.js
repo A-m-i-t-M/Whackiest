3eb129b99470f06d75105a6dc135917ddedd99c3
ox1g3n <aagnikg2003@gmail.com>
2024-12-21 16:17:15 +0530

    admin cancel booking api

--- Admin/backend/controllers/AdminBooking.controller.js
+++ Admin/backend/controllers/AdminBooking.controller.js
@@ -11,3 +11,39 @@ export const getAdminBookings = async (req, res) => {
     }
   }
   
+export const cancelBooking= async(req,res)=>{
+  try {
+    const { bookingId } = req.body;
+
+    if (!bookingId) {
+        return res.status(400).json({
+            success: false,
+            message: "booking ID is required.",
+        });
+    }
+
+    const darshan = await Booking.findOne({ _id: bookingId });
+
+    if (!darshan) {
+        return res.status(404).json({
+            success: false,
+            message: "booking not found or you don't have access to it.",
+        });
+    }
+
+    // Delete the snippet
+    await Booking.findByIdAndDelete(bookingId);
+
+    res.status(200).json({
+        success: true,
+        message: "booking deleted successfully.",
+    });
+} catch (error) {
+    console.error(error);
+    res.status(500).json({
+        success: false,
+        message: "Error deleting booking.",
+        error: error.message,
+    });
+}
+}
--- Admin/backend/routes/admin.booking.route.js
+++ Admin/backend/routes/admin.booking.route.js
@@ -1,9 +1,9 @@
 import express from "express";
 import {authMiddleware} from '../middleware/middleware.js'
-import {getAdminBookings} from '../controllers/AdminBooking.controller.js'
+import {getAdminBookings,cancelBooking} from '../controllers/AdminBooking.controller.js'
 
 const router = express.Router();
 
 router.get('/admin',authMiddleware,getAdminBookings);
-
+router.post('/admin/cancel',authMiddleware,cancelBooking)
 export default router;
